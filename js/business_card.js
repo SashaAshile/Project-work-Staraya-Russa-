9bed81fd3d45257370fc246d7571d5d426c939b2
Danll21Ascor <128476990+SashaAshile@users.noreply.github.com>
2023-04-16 21:38:22 +0300

    доработки

--- js/business_card.js
+++ js/business_card.js
@@ -2,3 +2,8 @@
 function description_active() {
 	document.querySelector(".description").classList.toggle("description_active");
 }
+
+function button_order_a_website_active() {
+	document.querySelector(".button_order_a_website").classList.toggle("button_order_a_website_active");
+	document.querySelector(".ism").classList.toggle("ism_active");	
+}
