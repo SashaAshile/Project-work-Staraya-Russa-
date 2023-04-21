bc084b96d91006a40a8f859f1b6a785f96623bee
Danll21Ascor <128476990+SashaAshile@users.noreply.github.com>
2023-04-21 13:54:34 +0300

    погода

--- js/business_card.js
+++ js/business_card.js
@@ -7,3 +7,31 @@ function button_order_a_website_active() {
 	document.querySelector(".button_order_a_website").classList.toggle("button_order_a_website_active");
 	document.querySelector(".link_moving").classList.toggle("link_moving_active");	
 }
+
+
+
+
+
+var bodyRect = document.body.getBoundingClientRect();
+var blocks = document.querySelector(".persone_blocks");
+var writing = document.querySelector(".writing");
+
+blocks.addEventListener("mouseover", function() {
+	var elemRect = document.querySelector(".setting_position").getBoundingClientRect()
+    var offsetX   = elemRect.top - bodyRect.top;
+    var offsetY   = elemRect.left - bodyRect.left;
+
+	
+	writing.classList.toggle("writing_active");	
+
+	writing.style.top = `${offsetX}px`;
+	writing.style.left = `${offsetY}px`;
+});
+
+
+blocks.addEventListener("mouseout", function() {
+	writing.classList.toggle("writing_active");		
+});
+
+
+
